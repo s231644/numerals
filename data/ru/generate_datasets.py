0f2f31afc3ac3d71414a5fde086d06f9bdb98366
Daniil Vodolazsky <32366948+s231644@users.noreply.github.com>
2020-02-06 23:39:12 +0100

    Added datasets
    Added datasets

---
+++ data/ru/generate_datasets.py
@@ -0,0 +1,132 @@
+from utils import *
+import numpy as np
+
+basic_numbers = dict()
+str2tree_num = dict()
+
+with open('ru_numerals_base.conllu', 'r') as f:
+    current_number = []
+    current_tokens = []
+    root_number = 0
+    is_basic = True
+    for line in f.readlines()[1:]:
+        l = line.strip().split('\t')
+        if len(l) <= 1:
+            if root_number > 0:
+                str2tree_num[' '.join(current_number)] = Tree(current_tokens)
+                current_tokens = []
+            if is_basic:
+                basic_numbers[root_number] = current_number
+            current_number = []
+            root_number = 0
+            continue
+        token = l[1]
+        if l[9]:
+            token += '_'
+        current_tokens.append(Token(l[0], token, l[2], l[3], l[4], l[5], l[6], l[7], l[10], 0))
+        if len(l) > 11:
+            root_number = int(l[11])
+            is_basic = len(l) <= 12
+        current_number.append(token)
+
+
+def convert_integer_to_tree(n):
+  if n == 0:
+    return Tree([])
+  if n in basic_numbers:
+    # 1 ... 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1000000
+    numeral = basic_numbers[n]
+    return deepcopy(str2tree_num[' '.join(numeral)])
+  if n < 100:
+    # 21 ... 99
+    tree_l = convert_integer_to_tree(n - (n % 10))
+    tree_r = convert_integer_to_tree(n % 10)
+    return merge_trees(tree_l, tree_r, is_arc_l2r=False, arc_label='add')
+  if n < 1000:
+    # 101 ... 999
+    tree_l = convert_integer_to_tree(n - (n % 100))
+    tree_r = convert_integer_to_tree(n % 100)
+    return merge_trees(tree_l, tree_r, is_arc_l2r=False, arc_label='add')
+  if n < 1000000:
+    tree_l = convert_integer_to_tree(n // 1000)
+    tree_m = convert_integer_to_tree(1000)
+    if tree_l.numeral[-1] == 'один':
+      tree_l.tokens[-1].form = 'одна'
+    elif tree_l.numeral[-1] == 'два':
+      tree_l.tokens[-1].form = 'две'
+      tree_m.tokens[-1].form = 'тысячи'
+    elif tree_l.numeral[-1] in {'три', 'четыре'}:
+      tree_m.tokens[-1].form = 'тысячи'
+    else:
+      tree_m.tokens[-1].form = 'тысяч'
+    tree_lm = merge_trees(tree_l, tree_m, is_arc_l2r=False, arc_label='mult')
+    if n % 1000:
+      tree_r = convert_integer_to_tree(n % 1000)
+      return merge_trees(tree_lm, tree_r, is_arc_l2r=False, arc_label='add')
+    else:
+      return tree_lm
+  if n < 1000000000:
+    tree_l = convert_integer_to_tree(n // 1000000)
+    tree_m = convert_integer_to_tree(1000000)
+    if tree_l.numeral[-1] == 'один':
+      pass
+    elif tree_l.numeral[-1] in {'два', 'три', 'четыре'}:
+      tree_m.tokens[-1].form = 'миллиона'
+    else:
+      tree_m.tokens[-1].form = 'миллионов'
+    tree_lm = merge_trees(tree_l, tree_m, is_arc_l2r=False, arc_label='mult')
+    if n % 1000000:
+      tree_r = convert_integer_to_tree(n % 1000000)
+      return merge_trees(tree_lm, tree_r, is_arc_l2r=False, arc_label='add')
+    else:
+      return tree_lm
+
+
+def convert_sum(x, y):
+    tree_l = convert_integer_to_tree(x)
+    tree_m = Tree([Token(1, 'плюс', 'плюс', 'CONJ', 'CONJ', '', 0, 'root', 0, 0)])
+    tree_r = convert_integer_to_tree(y)
+    tree_lm = merge_trees(tree_l, tree_m, is_arc_l2r=False, arc_label='larg')
+    return merge_trees(tree_lm, tree_r, is_arc_l2r=True, arc_label='rarg'), convert_integer_to_tree(x+y)
+
+
+def convert_prod(x, y):
+    tree_l = convert_integer_to_tree(x)
+    tree_m = Tree([Token(1, 'на', 'на', 'CONJ', 'CONJ', '', 0, 'root', 1, 1)])
+    tree_r = convert_integer_to_tree(y)
+    tree_lm = merge_trees(tree_l, tree_m, is_arc_l2r=False, arc_label='larg')
+    return merge_trees(tree_lm, tree_r, is_arc_l2r=True, arc_label='rarg'), convert_integer_to_tree(x*y) 
+
+
+src = open('src_add.txt', 'w')
+trg = open('trg_add.txt', 'w')
+
+max_range = 100
+for i in range(1000):
+    x = 1 + np.random.randint(max_range - 1)
+    y = 1 + np.random.randint(max_range - 1)
+    z = x + y
+    inp, out = convert_sum(x, y)
+    s = f'# {x} + {y}\n' + '\n'.join([t.__repr__() for t in inp.tokens]) + '\n\n'
+    t = f'# {z}\n' + '\n'.join([t.__repr__() for t in out.tokens]) + '\n\n'
+    src.writelines([s])
+    trg.writelines([t])
+src.close()
+trg.close()
+
+
+src = open('src_mult.txt', 'w')
+trg = open('trg_mult.txt', 'w')
+
+max_range = 100
+for i in range(1000):
+    x = 1 + np.random.randint(max_range - 1)
+    y = 1 + np.random.randint(max_range - 1)
+    z = x * y
+    inp, out = convert_prod(x, y)
+    s = f'# {x} * {y}\n' + '\n'.join([t.__repr__() for t in inp.tokens]) + '\n\n'
+    t = f'# {z}\n' + '\n'.join([t.__repr__() for t in out.tokens]) + '\n\n'
+    src.writelines([s])
+    trg.writelines([t])
+src.close()
+trg.close()
---
+++ data/ru/utils.py
@@ -0,0 +1,52 @@
+from copy import deepcopy
+
+class Token:
+    def __init__(self, position, form, lemma, pos, pos_phrase, tags, parent, label, value, value_phrase=None):
+        self.position = int(position)
+        self.form = form
+        self.lemma = lemma
+        self.pos = pos
+        self.pos_phrase = pos_phrase
+        self.tags = set(tags.split('|'))
+        self.parent = int(parent)
+        self.label = label
+        self.value = int(value)
+        self.value_phrase = int(value_phrase)
+
+    def __str__(self):
+        return f'{self.position}\t{self.form}\t{self.lemma}\t{self.pos}\t{self.parent}\t{self.label}\t{self.value}'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Tree:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.numeral = [t.form for t in self.tokens]
+        self.arcs = [(t.parent, t.label) for t in self.tokens]
+        for i, token in enumerate(self.tokens):
+            if (token.parent, token.label) == (0, 'root'):
+                self.root = i
+
+    def __str__(self):
+        return ' '.join(self.numeral) + ' ' + ' '.join([f'{parent}:{label}' for (parent, label) in self.arcs])
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def merge_trees(tree_l, tree_r, is_arc_l2r=True, arc_label='add'):
+    tokens_l = deepcopy(tree_l.tokens)
+    tokens_r = deepcopy(tree_r.tokens)
+    for t in tokens_r:
+        t.position += len(tree_l.tokens)
+        if t.parent > 0:
+            t.parent += len(tree_l.tokens)
+    if is_arc_l2r:
+        tokens_r[tree_r.root].parent = tokens_l[tree_l.root].position
+        tokens_r[tree_r.root].label = arc_label
+    else:
+        tokens_l[tree_l.root].parent = tree_r.tokens[tree_r.root].position + len(tree_l.tokens)
+        tokens_l[tree_l.root].label = arc_label
+    return Tree(tokens_l + tokens_r)
